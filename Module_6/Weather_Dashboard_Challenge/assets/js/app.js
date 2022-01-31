887fd8413189506ea4103ccc1b4624985b8c97cd
Stephen Carlin <scarlinj@gmail.com>
2022-01-30 22:28:21 -0800

    fixed display weather

--- Module_6/Weather_Dashboard_Challenge/assets/js/app.js
+++ Module_6/Weather_Dashboard_Challenge/assets/js/app.js
@@ -1,19 +1,25 @@
 // OpenWeather.org, generated API key = b50f611b32ae342cc1fd60d8843c9a81
 
-var apiKey = 'b50f611b32ae342cc1fd60d8843c9a81';
 
-
-var apiUrl = `api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid=${apiKey}`;
 // to pass API key
 var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var searchZip = document.querySelector("#search-zip");
+var weatherResult = document.querySelector('#weather');
 
 var getWeatherRepos = function (zip) {
+    var apiKey = 'b50f611b32ae342cc1fd60d8843c9a81';
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`;
     fetch(apiUrl).then(function (response) {
             // response was successful
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayRepos(data, zip);
+                    var city = document.createElement("h3");
+                    var temp = document.createElement("p");
+                    weatherResult.appendChild(city);
+                    weatherResult.appendChild(temp);
+                    city.textContent = data.name;
+                    temp.textContent = data.main.temp;
+                    console.log(data);
                 });
             } else {
                 console.log(response);
@@ -26,15 +32,20 @@ var getWeatherRepos = function (zip) {
         });
 };
 
-// original fetch code, before update after update 6.3
-fetch(url)
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (weatherResponse) {
-        console.log(weatherResponse);
-    });;
+searchZip.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var zip = document.querySelector('#zipValue').value;
+    getWeatherRepos(zip)
+})
+// original fetch code, before update after update 6.3:
+// fetch(apiUrl)
+//     .then(function (response) {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then(function (weatherResponse) {
+//         console.log(weatherResponse);
+//     });;
 
 // 
 // .search {
@@ -47,21 +58,21 @@ fetch(url)
 //     font - size: 17 px;
 // }
 
-fetch(apiUrl).then(function (response) {
-        // response was successful
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayRepos(data, user);
-            });
-        } else {
-            alert("Error: Github user not found.");
-        }
-        // console.log(data);
-    })
-    .catch(function (error) {
-        //Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
-    });
+// fetch(apiUrl).then(function (response) {
+//         // response was successful
+//         if (response.ok) {
+//             response.json().then(function (data) {
+//                 displayRepos(data, user);
+//             });
+//         } else {
+//             alert("Error: Github user not found.");
+//         }
+//         // console.log(data);
+//     })
+//     .catch(function (error) {
+//         //Notice this `.catch()` getting chained onto the end of the `.then()` method
+//         alert("Unable to connect to GitHub");
+//     });
 
 
 var button = document.getElementsByClassName('button');
